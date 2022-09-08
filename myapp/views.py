168f475842b65ee766c9534380cbefb10162c309
Md. Asadozzaman <83003038+asadozzaman@users.noreply.github.com>
2022-09-08 12:17:38 +0600

    second

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.shortcuts import render
+
+
+def hello(request, name):
+    return render(request, "hello.html", {"name": name})
+
+    # return HttpResponse("<b>Hello " + name +"</b>")
